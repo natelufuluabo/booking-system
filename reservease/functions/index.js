7620981771908575794cb0131e2ba5836bfecfa5
aircongo <natelufuluabo@yahoo.ca>
2024-03-08 17:32:25 -0500

    functions to check email completed

--- reservease/functions/index.js
+++ reservease/functions/index.js
@@ -1,49 +1,24 @@
 const {onRequest} = require("firebase-functions/v2/https");
 const {app} = require("./firebase_config");
 const {
-  getFirestore, collection,
-  addDoc, getDocs, where, query,
+  getFirestore, collection, addDoc,
+  getDocs, where, query,
 } = require("firebase/firestore");
 
-exports.createRestaurantAccount = onRequest({cors: true}, async (req, res) => {
+exports.checkIfEmailAlreadyUsed = onRequest({cors: true}, async (req, res) => {
   const db = getFirestore(app);
-  const emailCheckResponse = await checkIfEmailAlreadyUsed(req.body.email);
-  if (emailCheckResponse.message === "Email already in use") {
-    res.status(400).json({message: emailCheckResponse.message});
-    return;
-  }
-  if (emailCheckResponse.message === "Error checking email existence") {
-    res.status(500).json({message: "Error creating account. Retry"});
-    return;
-  }
-  try {
-    const docRef = await addDoc(collection(db, "restaurants"), req.body);
-    res.status(204).json({accountID: docRef.id});
-  } catch (e) {
-    res.status(500).json({message: "Error creating account. Retry"});
-    return;
-  }
-});
-
-/**
- * Checks if the given email is already in use
- * in the "restaurants" collection of the Firestore database.
- * @async
- * @function checkIfEmailAlreadyUsed
- * @param {string} email - The email to check for existence in the database.
- * @return {Promise<{message: string}>} A promise that resolves to an object
- * with a message indicating the result of the email check.
- * @throws {Error} If there is an error while checking the email existence.
- */
-async function checkIfEmailAlreadyUsed(email) {
-  const db = getFirestore(app);
-  const q = query(collection(db, "restaurants"), where("email", "==", email));
+  const q = query(
+      collection(db, "restaurants"), where("email", "==", req.body.email));
   try {
     const querySnapshot = await getDocs(q);
-    if (querySnapshot.size > 0) return {message: "Email already in use"};
-    else return {message: "Email not already in use"};
+    if (querySnapshot.size > 0) {
+      return res.status(401).json({code: 401});
+    } else {
+      return res.status(200).json({code: 200});
+    }
   } catch (error) {
-    return {message: "Error checking email existence"};
+    return res.status(500).json(
+        {code: 500});
   }
-}
+});
 
