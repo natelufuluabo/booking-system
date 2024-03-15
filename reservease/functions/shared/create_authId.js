26775417c9d10b77533c6d590ac6a1ac8a1c87b2
aircongo <natelufuluabo@yahoo.ca>
2024-03-15 17:21:38 -0400

    created shared folders for functions that will be re-used in other functions

---
+++ reservease/functions/shared/create_authId.js
@@ -0,0 +1,23 @@
+const {getAuth, createUserWithEmailAndPassword} = require("firebase/auth");
+const {app} = require("../firebase_config");
+
+/**
+ * Creates a new authentication ID (UID)
+ * for a user with the provided email and password.
+ * @param {string} email - The email of the user.
+ * @param {string} password - The password of the user.
+ * @return {Promise<string|Error>} - A promise that resolves to the UID of the
+ * newly created user, or an error if the operation fails.
+ */
+async function createAuthID(email, password) {
+  try {
+    const auth = getAuth(app);
+    const userCredential = await createUserWithEmailAndPassword(
+        auth, email, password);
+    return userCredential.user.uid;
+  } catch (error) {
+    return error;
+  }
+}
+
+exports.module = createAuthID;
