84873c9efc4d868a716079a0849a0ece4eef64b4
aircongo <natelufuluabo@yahoo.ca>
2024-03-20 19:45:19 -0400

    createAdmin completed and deployed successfully

--- reservease/functions/index.js
+++ reservease/functions/index.js
@@ -2,9 +2,12 @@ const checkIfEmailAlreadyUsed = require("./functions/email_check");
 const createRestaurantAccount = require(
     "./functions/restaurant_account_creation");
 const createSuperUser = require("./functions/superUser_account_creation");
+const createAdmin = require("./functions/admin_account_creation");
 
 exports.checkIfEmailAlreadyUsed = checkIfEmailAlreadyUsed;
 
 exports.createRestaurantAccount = createRestaurantAccount;
 
 exports.createSuperUser = createSuperUser;
+
+exports.createAdmin = createAdmin;
